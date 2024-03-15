de696f62225875caa891d970e7fd5b9e1444071f
zaijan <jgodito27@gmail.com>
2024-03-15 22:55:25 +0800

    finalize

--- src/app/views/CapstoneGenerator.tsx
+++ src/app/views/CapstoneGenerator.tsx
@@ -1,17 +1,21 @@
-'use client'
+"use client";
 import * as React from "react";
 import { useEffect, useState } from "react";
-import NextButton from "../components/Button";
-import { randomTitles } from "../api/randomTitles";
-import RandomTitle from "../components/RandomTitle";
-import SkeletonTitle from "../components/skeleton/SkeletonTitle"; 
+import { useTheme } from "@mui/material/styles";
 import { Box, Typography, Fade } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { randomTitles } from "../contant/randomTitles";
+import RandomTitle from "../components/RandomTitle";
+import SkeletonTitle from "../components/skeleton/SkeletonTitle";
+import NextButton from "../components/Button";
 import Title from "../components/Title";
 
 export default function CapstoneGenerator() {
   const [title, setTitle] = useState<string>("");
-  const [isLoading, setIsLoading] = useState(true);
-  const [animateTitle, setAnimateTitle] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [animateTitle, setAnimateTitle] = useState<boolean>(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const generateTitle = () => {
     const randomIndex = Math.floor(Math.random() * randomTitles.length);
@@ -29,20 +33,30 @@ export default function CapstoneGenerator() {
     setIsLoading(true);
     setAnimateTitle(false);
     setTitle("");
-    setTimeout(generateTitle, 500); 
+    setTimeout(generateTitle, 500);
   };
 
   return (
-    <Box sx={{ textAlign: "center", minHeight: 200 }} component="div">
-      <Title/>
+    <Box
+      sx={{
+        textAlign: "center",
+        minHeight: 200,
+        maxWidth: isMobile ? 300 : "none",
+        padding: isMobile ? 0 : 9,
+        my: 3,
+        mx: "auto",
+      }}
+      component="div"
+    >
+      <Title />
       <Typography
         variant="subtitle1"
         component="span"
-        sx={{ color: "#121212", display: "flex", textAlign: "center" }}
+        sx={{ color: "#121212", display: "flex", textAlign: "center", fontWeight: "bold", my: 3, maxWidth: 900, mx: "auto"}}
       >
-        This application generates study title ideas at random for you to use as
-        a guide or source of inspiration. At the moment, majors in computer
-        science, information technology and related fields, are the target audience for the created titles.
+        This basic project tool will assist other students studying information
+        technology, computer science, or any related discipline in coming up
+        with a title for their capstone.
       </Typography>
       <NextButton onClick={handleNextTitle} loading={isLoading} />
       {isLoading ? (
